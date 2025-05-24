d3c1a998d618268a34066c5f19e307a9303e92cc
Pablo Alan <pabloxt14@gmail.com>
2025-05-24 19:00:00 -0300

    chore: add lint (biomejs)

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar'
+
+import { Home } from './app/home'
+
+export default function App() {
+  return (
+    <>
+      <Home />
+      <StatusBar style="auto" />
+    </>
+  )
+}
